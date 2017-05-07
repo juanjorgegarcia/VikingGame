f487025d35a40224ee0548badba9a5ac91ad4ea2
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-05-07 15:12:53 -0300

    Codigo comentado e organizaDO, melhorado a impletemtação da classe

--- vikingpygame.py
+++ vikingpygame.py
@@ -5,27 +5,29 @@ pygame.init()
 class Player:
     #classe para Player
     def __init__(self,x,y,sprite,speed_):
-        self.x = x
-        self.y = y
-        self.sprite=pygame.image.load(sprite)
-
-        self.speed_x = 0
-        self.speed_y = 0
-        self.aceleration = + 0.2
-        self.walkR = False
-        self.walkL = False
-        self.current_img = pygame.image.load(sprite)
-        self.rect=pygame.Surface.get_size(self.current_img)
+        self.x = x #coordenada x do personagem
+        self.y = y #coordenada y do personagem
+        self.sprite=pygame.image.load(sprite) # carregando o sprite parada do player
+        self.speed_x = 0 #velocidade no eixo x
+        self.speed_y = 0 #velocidade no eixo x
+        self.aceleration = + 0.2 #gravidade
+        self.walkR = False #status do player andando
+        self.walkL = False #status do player andando
+        self.current_img = pygame.image.load(sprite) # sprite atual o player
+        self.rect=pygame.Surface.get_size(self.current_img) #retangulo equivalente a sprite
     def move(self,direction):
 
         if direction == "left":
             self.speed_x = 10
-            char1.walkL=True
+            self.walkL=True
         if direction == "right":
             self.speed_x = 10
-            char1.walkR=True
+            self.walkR=True
         if direction == 0:
             self.speed_x = 0
+            #self.walkL=False
+            #self.walkR=False
+
         if direction == "up":
             self.speed_y = -10 + self.aceleration
             global jump
@@ -35,20 +37,19 @@ class Player:
         self.speed_y+=self.aceleration
 
         if self.y >400:
+            #  se a posicao y do player estiver abaixo Do chao a velocidade_y dele se torna 0
             self.speed_y = 0
             global jump
-            jump=0
-            self.y=399
+            jump=0 #impedindo o jump infinito
+            self.y=399 #%.99999999999999
         self.y += self.speed_y
 
         if self.walkR == True:
             self.x += self.speed_x
 
-
         if self.walkL == True:
             if self.x > 0:
                 self.x -= self.speed_x
-
 
 
 
@@ -61,9 +62,9 @@ screen=pygame.display.set_mode((1280,720))
 background = pygame.image.load("8bitvapor.png") #dando load
 background = pygame.transform.scale(background, (1280, 720))  #escalano conforme a tela
 background = background.convert() #covertenod os pixels da imagem (a imagem é carregada mais rapidamete)
-#%######%##
+######
 jump=0
-###%######## carregando as sprites do player
+############ carregando as sprites do player
 char1=Player(400,400,player1,10)
 pwalkright0=pygame.image.load("Images\\walk right\\sprite_walkR0.png")#dando load
 pwalkright0 = pygame.transform.scale(pwalkright0, (char1.rect))#escalano conforme o tamanho personagem
@@ -73,24 +74,22 @@ pwalkright2=pygame.image.load("Images\\walk right\\sprite_walkR2.png")#dando loa
 pwalkright2 = pygame.transform.scale(pwalkright2, (char1.rect))#escalano conforme o tamanho personagem
 pwalkright3=pygame.image.load("Images\\walk right\\sprite_walkR3.png")#dando load
 pwalkright3 = pygame.transform.scale(pwalkright3, (char1.rect))#escalano conforme o tamanho personagem
-
-
-
+############
 
 pygame.display.set_caption("A Tale of the Unworthy") #Titulo da janela do jogo
 running=True
+############
 while running:
-    screen.blit(background, (0, 0))
-    screen.blit(char1.current_img,(char1.x,char1.y))
-    pygame.display.update()
-    for event in pygame.event.get():
+    screen.blit(background, (0, 0)) ### pintando o background
+    screen.blit(char1.current_img,(char1.x,char1.y)) ### pintando o player
+    pygame.display.update()### atualizando o display
+    for event in pygame.event.get(): #pegando as açoes do usuario
         if event.type == pygame.QUIT:
-            running=False
+            running=False #saindo do jogo fechando a janela
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                running = False
+                running = False #saindo do jogo apertano esc
             if event.key == pygame.K_UP and jump==0:
-
                 char1.move("up")
             if event.key == pygame.K_RIGHT:
                 char1.move("right")
@@ -103,21 +102,9 @@ while running:
                 char1.walkL=False
 
 
-        # #if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-        #     #char1.walkR=True
-        # if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
-        #     char1.walkR=False
-        # # if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-        # #     char1.walkL=True
-        # if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
-        #     char1.walkL=False
-
-
-
-    #jump=0
-
-    char1.updatepos()
-    screen.blit(char1.current_img,(char1.x,char1.y))
+    char1.updatepos() ## atualizando a posicao do personagem
+    screen.blit(char1.current_img,(char1.x,char1.y))### pintando o player
     clock.tick(60)
-pygame.quit()
-quit()
+############
+pygame.quit() #fechando o pygame
+quit() # fechandoo python
