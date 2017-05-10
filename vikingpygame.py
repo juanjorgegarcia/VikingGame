343a094e5e1b130aa763e3adce68d22223f3cda8
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-05-09 23:35:48 -0300

    Adicionado a classe Blocks, o chao (plataforma do jogo) e o sistema de colisão quase completo

--- vikingpygame.py
+++ vikingpygame.py
@@ -1,24 +1,26 @@
 import pygame, os
-#print (os.getcwd())
+from numpy import arange
 pygame.init()
 
 class Player:
     #classe para Player
-    def __init__(self,x,y,sprite,speed_):
+    def __init__(self,x,y,sprite):
         self.x = x #coordenada x do personagem
         self.y = y #coordenada y do personagem
         self.sprite=pygame.image.load(sprite).convert() # carregando o sprite parada do player
         self.speed_x = 0 #velocidade no eixo x
         self.speed_y = 0 #velocidade no eixo x
-        self.aceleration = + 0.2 #gravidade
+        self.aceleration = + 0.4 #gravidade
         self.walkR = False #status do player andando para direita
         self.walkL = False #status do player andando para esquerda
         self.jump = False #status do player pulando
         self.rightface= True #sabaer pra onde o jogador esta olhando
-        self.leftface=False
-        self.current_img = pygame.image.load(sprite) # sprite atual o player
-        self.rect=pygame.Surface.get_size(self.current_img) #retangulo equivalente a sprite
-
+        self.leftface = False
+        self.current_img = pygame.image.load(sprite).convert_alpha() # sprite atual o player
+        self.size = pygame.Surface.get_size(self.current_img) #retangulo equivalente a sprite
+        self.rect = self.current_img.get_rect(x = self.x, y = self.y)
+        self.colision = False # personagem nao esta colidindo
+        self.hitbox = pygame.Rect(self.x,self.y,self.x+self.size[0],self.size[1]) #hitbox do personagem
     def move(self,direction):
         #movendo o player
         if direction == "left":
@@ -59,21 +61,31 @@ class Player:
             #self.walkR=False
 
         if direction == "up":
-            self.aceleration = 0.2 #se o personagem estiver no ar a aceleraçao esta valendo!
-            self.speed_y = -10 + self.aceleration
+            self.aceleration = 0.4 #se o personagem estiver no ar a aceleraçao esta valendo!
+            self.speed_y = -15 #+ self.aceleration
             self.jump = True #player esta pulando!
 
     def updatepos(self):
         #atualizando a posicao do player
         self.speed_y+=self.aceleration
 
-        if self.y >400:
-            #  se a posicao y do player estiver abaixo Do chao a velocidade_y e a gravidade dele se torna 0
-            self.speed_y = 0 #
-            self.aceleration = 0 #
-            self.jump=False #impedindo o jump infinito
-            self.y=400 #.99999999999999
         self.y += self.speed_y
+        self.rect = self.current_img.get_rect(x=self.x,y=self.y)
+        self.hitbox = pygame.Rect(self.x+70,self.y,60,192)
+        #print(self.hitbox)
+        for i in floor: #floor é a lista q contem todos os objetos da plataforma
+            if self.hitbox.colliderect(i.rect) == True: #se o retangulo do player colidir com o da plataforma
+                #print("colidindo")
+                self.speed_y = 0
+                self.aceleration = 0
+                self.jump = False
+                self.colision = True
+                break
+            else:
+                self.colision=False
+
+        if self.colision == False:
+            self.aceleration = 0.4
 
         if self.walkR == True:
             self.x += self.speed_x
@@ -84,28 +96,48 @@ class Player:
                 self.x -= self.speed_x
 
 
+class Blocks():
+    # classe para elementos estáticos do jogo
+
+    def __init__(self,x,y,sprite):
+        self.x = x
+        self.y = y
+        self.image = sprite
+        self.width,self.height = pygame.Surface.get_size(self.image)
+        self.rect = self.image.get_rect(x = self.x, y = self.y)
+
+class Menu():
+    #classe para o menu o jogo
+    #devera conter funções como, start, savegame, highscore, creditos e customizaçao (posivelmente)
+    pass
+
+
 ######
 clock=pygame.time.Clock() #importando o timer
 ######
 ######
-screen=pygame.display.set_mode((1280,720)) #criando o display do jogo
+screen_x=1280
+screen_y=720
+screen=pygame.display.set_mode((screen_x,screen_y)) #criando o display do jogo
 ######
 ###### carregando o background Do jogo
 background = pygame.image.load("8bitvapor.png") #dando load
-background = pygame.transform.scale(background, (1280, 720))  #escalano conforme a tela
+background = pygame.transform.scale(background, (screen_x, screen_y))  #escalano conforme a tela
 background = background.convert() #covertenod os pixels da imagem (a imagem é carregada mais rapidamete)
 ######
+ground0 = pygame.image.load("Images\\chão\\ground_middle.png").convert()
+ground0 = pygame.transform.scale(ground0,(100,100)).convert()
+ground = Blocks(800,450,ground0)
+groundRange =arange(0,screen_x,ground.width)
 ############ carregando as sprites do player
 player1="Images\\stand.png"
-char1=Player(400,400,player1,10)
-pwalkright0 = pygame.image.load("Images\\walk right\\sprite_walkR0.png").convert() #dando load
-pwalkright0 = pygame.transform.scale(pwalkright0, (char1.rect))#escalano conforme o tamanho personagem
-pwalkright1 = pygame.image.load("Images\\walk right\\sprite_walkR1.png").convert() #dando load
-pwalkright1 = pygame.transform.scale(pwalkright1, (char1.rect))#escalano conforme o tamanho personagem
-pwalkright2 = pygame.image.load("Images\\walk right\\sprite_walkR2.png").convert() #dando load
-pwalkright2 = pygame.transform.scale(pwalkright2, (char1.rect))#escalano conforme o tamanho personagem
-pwalkright3 = pygame.image.load("Images\\walk right\\sprite_walkR3.png").convert() #dando load
-pwalkright3 = pygame.transform.scale(pwalkright3, (char1.rect))#escalano conforme o tamanho personagem
+char1=Player(400,400,player1)
+char_spritedata = {}
+for i in range (3):
+    name = "pwalkright{}".format(i)
+    pwalkright = pygame.image.load("Images\\walk right\\sprite_walkR{}.png".format(i)).convert()
+    pwalkright = pygame.transform.scale(pwalkright, (char1.size))
+    char_spritedata[name] = pwalkright
 ############
 
 pygame.display.set_caption("A Tale of the Unworthy") #Titulo da janela do jogo
@@ -113,6 +145,12 @@ running=True
 ############
 while running:
     screen.blit(background, (0, 0)) ### pintando o background
+    screen.blit(ground.image,(ground.x,ground.y))
+    floor=[ground]
+    for i in groundRange:
+        chao = Blocks(i,390+char1.size[1],ground.image)
+        floor.append(chao)
+        screen.blit(ground.image,(i,390+(char1.size[1])))
     screen.blit(char1.current_img,(char1.x,char1.y)) ### pintando o player
     pygame.display.update()### atualizando o display
     for event in pygame.event.get(): #pegando as açoes do usuario
@@ -135,10 +173,8 @@ while running:
             if event.key == pygame.K_LEFT:
                 char1.move("stopleft")
 
-
-
-
     char1.updatepos() ## atualizando a posicao do personagem
+    floor=[ground]
     screen.blit(char1.current_img,(char1.x,char1.y))### pintando o player
     clock.tick(60) # ajustando o fps
 ############
