cac12ed3a022cfff88019943cde414015f831b70
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-06-02 01:57:00 -0300

    Sons melhorados, com um menor delay

--- vikingpygame.py
+++ vikingpygame.py
@@ -13,8 +13,10 @@ import time
 
 #######
 #initializing pygame
-pygame.init()
+pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
+pygame.init()
+
 #####
 
 class Player(pygame.sprite.Sprite):
